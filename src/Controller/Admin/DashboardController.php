0b5deef527d94d24234caf5fbebe2cd49e9d0b97
AdamaDiawara <diawaraad97@gmail.com>
2025-05-18 19:09:45 +0200

    After Admin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+
+#[AdminDashboard(routePath: '/admin', routeName: 'admin')]
+class DashboardController extends AbstractDashboardController
+{
+    public function index(): Response
+    {
+        //return $this->redirectToRoute('admin_user_index');
+         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+         return $this->redirect($adminUrlGenerator->setController(UserCrudController::class)->generateUrl());
+
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Reseau Social');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+}
---
+++ tests/SampleTest.php
@@ -0,0 +1,12 @@
+<?php
+// tests/SampleTest.php
+
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+    public function testAddition()
+    {
+        $this->assertEquals(4, 2 + 2);
+    }
+}
